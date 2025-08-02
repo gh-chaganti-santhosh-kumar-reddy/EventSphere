265ca13946847509520acbc96ce33fb38ed10f76
gh-chaganti-santhosh-kumar-reddy <chaganti.santhosh.kumar.reddy@ltim.trumio.ai>
2025-08-02 21:37:57 +0530

    commit

--- backend/obj/Debug/net8.0/project.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f175a416d119f69580b48a38c34d304d248fb53")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
